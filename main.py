3f4352f01f30c73916590ff797bd776133671c1c
Никита Баландин <nik.balandin.01@mail.ru>
2024-05-18 00:39:56 +0500

    Очищен файл main.py. В пакете test создан модуль module_testing для тестирования работоспособности модулей.

--- main.py
+++
@@ -1,7 +0,0 @@
-from Src import widget
-
-card_or_check_data = input("Введите данные счета/карты: ")
-unformated_operation_data = input("Введите дату проведенной операции: ")
-
-print(widget.mask_multifunction(card_or_check_data))
-print(widget.formatting_data_string(unformated_operation_data))
