5a356fb2992c436a641fc01b288b2df8c8465b9e
Никита Баландин <nik.balandin.01@mail.ru>
2024-05-18 00:31:00 +0500

    В модуль processing добавлена функция сортировки банковских операций по дате с возможностью выбора направления сортировки

---
+++ Src/processing.py
@@ -0,0 +1,16 @@
+def filter_of_bank_operation(operation_list: list, operation_status: str = "EXECUTED") -> list:
+    """Функция принимает на вход список банковских операций и параметр успешности операции.
+    Возвращает отсортированный список операций по критерию успешности проведения операции"""
+
+    filtered_operation_list = []
+    for dict in operation_list:
+        if dict["state"] == operation_status.upper():
+            filtered_operation_list.append(dict)
+    return filtered_operation_list
+
+
+def operation_date_sorting(operation_list: list, reverse_parameter: bool = False) -> list:
+    """Функция принимает на вход список банковских операций и параметр порядка сортировки.
+    Возвращает отсортированный список операций по дате проведения"""
+
+    return sorted(operation_list, key=lambda words: words["date"], reverse=reverse_parameter)
