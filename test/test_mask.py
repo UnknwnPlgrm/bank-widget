56d442a205a61e80922294ae2d593a4a281f7db5
Никита Баландин <nik.balandin.01@mail.ru>
2024-05-23 01:34:42 +0500

    Модули тестирования отформатированы линтерами

--- test/test_mask.py
+++ test/test_mask.py
@@ -1,22 +1,29 @@
 import pytest
+
 from Src import mask
 
-@pytest.mark.parametrize("card_number, mask_card_number", [
-    ("6475867563758697", "6475 86** **** 8697"),
-    ("64758e7563h5869z", "Неверно указан номер карты"),
-    ("64758675", "Неверно указан номер карты"),
-    ("", "Неверно указан номер карты")
-])
-def test_mask_number_of_card(card_number, mask_card_number):
+
+@pytest.mark.parametrize(
+    "card_number, mask_card_number",
+    [
+        ("6475867563758697", "6475 86** **** 8697"),
+        ("64758e7563h5869z", "Неверно указан номер карты"),
+        ("64758675", "Неверно указан номер карты"),
+        ("", "Неверно указан номер карты"),
+    ],
+)
+def test_mask_number_of_card(card_number: str, mask_card_number: str) -> None:
     assert mask.mask_number_of_card(card_number) == mask_card_number
 
 
-@pytest.mark.parametrize("check_number, mask_check_number", [
-    ("64686473678894779589", "**9589"),
-    ("646864736f889a7795b9", "Неверно указан номер счета"),
-    ("6468647", "Неверно указан номер счета"),
-    ("", "Неверно указан номер счета"),
-])
-def test_mask_check(check_number, mask_check_number):
+@pytest.mark.parametrize(
+    "check_number, mask_check_number",
+    [
+        ("64686473678894779589", "**9589"),
+        ("646864736f889a7795b9", "Неверно указан номер счета"),
+        ("6468647", "Неверно указан номер счета"),
+        ("", "Неверно указан номер счета"),
+    ],
+)
+def test_mask_check(check_number: str, mask_check_number: str) -> None:
     assert mask.mask_check(check_number) == mask_check_number
-
--- test/test_widget.py
+++ test/test_widget.py
@@ -1,24 +1,30 @@
 import pytest
+
 from Src import widget
 
 
-@pytest.mark.parametrize("card_or_check_data, masked_card_or_check_data", [
-    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-    ("Счет 64686473678894779589", "Счет **9589"),
-    ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
-    ("64686473678894779589", "Неверно указаны данные карты/счета"),
-    ("1596837868705199", "Неверно указаны данные карты/счета"),
-    ("", "Неверно указаны данные карты/счета")
-])
-def test_mask_multifunction(card_or_check_data, masked_card_or_check_data):
+@pytest.mark.parametrize(
+    "card_or_check_data, masked_card_or_check_data",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("64686473678894779589", "Неверно указаны данные карты/счета"),
+        ("1596837868705199", "Неверно указаны данные карты/счета"),
+        ("", "Неверно указаны данные карты/счета"),
+    ],
+)
+def test_mask_multifunction(card_or_check_data: str, masked_card_or_check_data: str) -> None:
     assert widget.mask_multifunction(card_or_check_data) == masked_card_or_check_data
 
 
-@pytest.mark.parametrize("input_data, formatted_data", [
-    ("2018-07-11T02:26:18.671407", "11.07.2018"),
-    ("2018-07-11T02:26:", "Неверно указаны данные операции"),
-    ("", "Неверно указаны данные операции"),
-
-])
-def test_formatting_data_string(input_data, formatted_data):
+@pytest.mark.parametrize(
+    "input_data, formatted_data",
+    [
+        ("2018-07-11T02:26:18.671407", "11.07.2018"),
+        ("2018-07-11T02:26:", "Неверно указаны данные операции"),
+        ("", "Неверно указаны данные операции"),
+    ],
+)
+def test_formatting_data_string(input_data: str, formatted_data: str) -> None:
     assert widget.formatting_data_string(input_data) == formatted_data
