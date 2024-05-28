70d167f18b67d488f8c11c6cb3b36e671427bbd9
Никита Баландин <nik.balandin.01@mail.ru>
2024-05-29 00:20:51 +0500

    добавлена новая фикстура в модуль conftest. добавлен код тестирования функций модуля generators.

---
+++ test/test_generators.py
@@ -0,0 +1,38 @@
+from Src import generators
+import pytest
+
+def test_generator_of_filtered_bank_operation(bank_operation_example_list: list) -> None:
+    generator_USD = generators.generator_of_filtered_bank_operation(bank_operation_example_list, "USD")
+    generator_RUB = generators.generator_of_filtered_bank_operation(bank_operation_example_list, "RUB")
+    assert next(generator_USD)["id"] == 939719570
+    assert next(generator_USD)["id"] == 142264268
+    assert next(generator_USD)["id"] == 895315941
+    assert next(generator_USD)["id"] == None
+    assert next(generator_RUB)["id"] == 873106923
+    assert next(generator_RUB)["id"] == 594226727
+    assert next(generator_RUB)["id"] == None
+
+def test_generator_of_description_of_operation(bank_operation_example_list: list) -> None:
+    generator_description = generators.generator_of_description_of_operation(bank_operation_example_list)
+    assert next(generator_description) == "Перевод организации"
+    assert next(generator_description) == "Перевод со счета на счет"
+    assert next(generator_description) == "Перевод со счета на счет"
+    assert next(generator_description) == "Перевод с карты на карту"
+    assert next(generator_description) == "Перевод организации"
+    assert next(generator_description) == None
+
+
+def test_generator_card_number() -> None:
+    generator_card_number = generators.generator_card_number(1, 5)
+    generator_card_number_two = generators.generator_card_number(99, 94)
+    generator_card_number_three = generators.generator_card_number(9999999999999999, 99999999999999992)
+    generator_card_number_four = generators.generator_card_number(0, 0)
+    assert next(generator_card_number) == "0000 0000 0000 0001"
+    assert next(generator_card_number) == "0000 0000 0000 0002"
+    assert next(generator_card_number) == "0000 0000 0000 0003"
+    assert next(generator_card_number) == "0000 0000 0000 0004"
+    assert next(generator_card_number) == None
+    assert next(generator_card_number_two) == None
+    assert next(generator_card_number_three) == "9999 9999 9999 9999"
+    assert next(generator_card_number_three) == "Неправильно указан диапазон"
+    assert next(generator_card_number_four) == None
