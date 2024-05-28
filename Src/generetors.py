ac404d7f61e0c7cfeb9a3cb392dc79549dad776d
Никита Баландин <nik.balandin.01@mail.ru>
2024-05-28 23:22:45 +0500

    в модуль generators добавлены функции-генераторы, согласно заданию

--- Src/generetors.py
+++ Src/generetors.py
@@ -1,93 +1,37 @@
-transactions = [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-]
+from collections.abc import Generator
 
 
-def generator_of_filtered_bank_operation(operation_list, currency):
+def generator_of_filtered_bank_operation(operation_list: list, currency: str) -> Generator[dict, str, None]:
+    """Функция-генератор принимает на вход список банковских операций и тип валюты для сортировки.
+    Возвращает, по вызову, отфильтрованную по типу валюты, банковскую операцию"""
+
     for operation in operation_list:
         if operation["operationAmount"]["currency"]["code"] == currency:
-                yield operation
+            yield operation
 
-def generator_of_description_of_operation(operation_list):
+
+def generator_of_description_of_operation(operation_list: list) -> Generator[str, int, None]:
+    """Функция-генератор принимает на вход список банковских операций.
+    Возвращает, по вызову, описание банковской операции"""
+
     for operation in operation_list:
         yield operation["description"]
 
-operations = generator_of_filtered_bank_operation(transactions, "USD")
-operations_desc = generator_of_description_of_operation(transactions)
 
-for i in range(10):
-    print(next(operations_desc))
+def generator_card_number(start: int, stop: int) -> Generator[str, int, None]:
+    """Функция-генератор принимает на вход диапазон чисел для генерации номера карты.
+    Возвращает, по вызову, сгенерированный номер карты по заданному диапазону"""
+
+    for x in range(start, stop):
+        quantity_of_numbers_add = 16 - len(str(x))
+        unformated_card_number = ("0" * quantity_of_numbers_add) + str(x)
+        card_number = (
+            unformated_card_number[0:4]
+            + " "
+            + unformated_card_number[4:8]
+            + " "
+            + unformated_card_number[8:12]
+            + " "
+            + unformated_card_number[12:16]
+        )
+        yield card_number
